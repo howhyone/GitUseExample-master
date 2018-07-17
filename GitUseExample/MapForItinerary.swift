89a8356b680ca1c96db03bf1f9522d582068f9e0
张旭 <mob@chongzige.local>
2018-07-17 16:33:33 +0800

    Map

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by mob on 2018/7/17.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
